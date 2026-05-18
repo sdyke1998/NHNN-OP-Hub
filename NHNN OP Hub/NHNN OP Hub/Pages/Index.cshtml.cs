fa5150a2d8841f2311650e3fe1f7f4226ae0b805
Scott Dyke <sdyke1998@gmail.com>
2026-05-18 21:00:40 +0100

    First commit

---
+++ NHNN OP Hub/NHNN OP Hub/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHNN_OP_Hub.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ NHNN OP Hub/NHNN OP Hub/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHNN_OP_Hub.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
