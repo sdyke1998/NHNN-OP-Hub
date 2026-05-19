e53ac89d01cf2efee60494f12034f10970de495d
Scott Dyke <sdyke1998@gmail.com>
2026-05-19 21:01:55 +0100

    Added a skeleton for the Outpatients Hub

---
+++ NHNN OP Hub/NHNN OP Hub/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHNN_OP_Hub.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NHNN OP Hub/NHNN OP Hub/Pages/Outpatients.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHNN_OP_Hub.Pages
+{
+    public class OutpatientsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
